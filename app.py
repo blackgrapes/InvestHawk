297454589d95f02f3af7d737c28cd30d696a0767
Jhalak09 <jhalakchhapola1@gmail.com>
2025-03-20 13:28:24 +0530

    Update app.py
    features and login page render

--- app.py
+++ app.py
@@ -13,9 +13,21 @@ def media(filename):
 def home():
     return render_template('home.html')
 
+
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/features.html')
+def features():
+    return render_template('features.html')
+    
+    
+    
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
